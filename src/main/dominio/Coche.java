a2fa8291ed1cce9ebaf8dfdfacf5cf88f3998de6
Pablo Rubio <prubioam@zubirimanteo.com>
2022-06-23 11:26:04 +0200

    constructor de coche creado

--- src/main/dominio/Coche.java
+++ src/main/dominio/Coche.java
@@ -4,5 +4,11 @@ public class Coche {
 
     private String id;
     private String modelo;
+
+    public Coche(String id, String modelo)
+    {
+        this.id = id;
+        this.modelo = modelo;
+    }
     
 }
